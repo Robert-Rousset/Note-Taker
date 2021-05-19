06087520719805c6fd1ed7479d08a31c1cae8a21
Robert Rousset <whyisrob@hotmail.com>
2021-05-20 00:02:00 +0800

    got server to get index page and notes page

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 
@@ -7,3 +6,10 @@ const PORT = 3000;
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+
+server.get('/notes', (request, response) => response.sendFile(path.join(__dirname, "../../notes.html")));
+
+server.get('*', (request, response) => response.sendFile(path.join(__dirname, '../../index.html')));
+
+
+server.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));

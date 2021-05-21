c32b23f8246a8cd3aea49073ad99510bad33c948
Robert Rousset <whyisrob@hotmail.com>
2021-05-21 10:30:18 +0800

    nearly finished, cant figure out how to get api/notes get request working

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
+let allNotes = require('../../../db/db.json')
 
 const server = express();
 const PORT = 3000;
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(express.static(path.join(__dirname, "../../../public")))
 
 server.get('/notes', (request, response) => response.sendFile(path.join(__dirname, "../../notes.html")));
 
 server.get('*', (request, response) => response.sendFile(path.join(__dirname, '../../index.html')));
 
-server.get('/api/notes', (request, response) => {
-    
 
+
+server.post('/api/notes', (request, response) =>{
+
+    let note = request.body
+
+    allNotes.push(note)
+
+    console.log(allNotes)
+    response.end()
 })
 
-
-
+server.get('/api/notes', (request, response) => {
+    response.sendFile(allNotes)
+})
 
 server.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
